586a1fada42e092cbb5423396e5d71d8fbd76dbc
lachy75127873-oss <lachy75127873@gmail.com>
2025-10-30 12:55:01 +0900

    feat: 보석 구현 시작

---
+++ Assets/Scripts/Obstacles Gimmick/Jem.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jem : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        
+    }
+}
