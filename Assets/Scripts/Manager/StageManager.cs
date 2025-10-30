5ecd9f8f3a984beedab2cc9edd779f8dad894aa1
shin0112 <wnslcosltimo12@gmail.com>
2025-10-30 20:05:56 +0900

    feat: stage manager Init 만들기
    - 스테이지 매니저가 모든 stage controller 컴포넌트를 찾아서 정보를 저장해둠.
    - 스테이지 선택을 하기 위함.

--- Assets/Scripts/Manager/StageManager.cs
+++ Assets/Scripts/Manager/StageManager.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public enum GameState
@@ -12,8 +13,8 @@ public enum GameState
 public class StageManager : MonoBehaviour
 {
     private GameManager _gameManager;
-    [SerializeField] private GameObject[] _stages;
-    private GameObject _stage;
+    private Dictionary<int, StageController> _stages = new();
+    private StageController _currentStage = null;
 
 
     private GameState _currentGameState = GameState.None;
@@ -22,5 +23,25 @@ public class StageManager : MonoBehaviour
     public void Init(GameManager gameManager)
     {
         _gameManager = gameManager;
+
+        var stages = GetComponentsInChildren<StageController>(true);    // 비활성화된 object도 탐색
+
+        _stages.Clear();
+
+        foreach (var stage in stages)
+        {
+            int id = stage.StageId;
+
+            if (_stages.ContainsKey(id))
+            {
+                Debug.Log($"[StageManager.Init] {id} 번째 스테이지 중복 할당");
+                continue;
+            }
+
+            _stages[id] = stage;
+            stage.gameObject.SetActive(false);
+        }
+
+        Debug.Log($"[StageManager.Init] 등록된 Stage 수: {_stages.Count}");
     }
 }
