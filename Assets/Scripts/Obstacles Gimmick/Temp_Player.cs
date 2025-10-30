ee4ee9afc61a3ba9ce6e62f44aac5daef5778ff3
lachy75127873-oss <lachy75127873@gmail.com>
2025-10-30 12:21:56 +0900

    Chore:사문화 코드 삭제
    임시 플레이어 코드 중 이동제어 담당 부분 삭제

--- Assets/Scripts/Obstacles Gimmick/Temp_Player.cs
+++ Assets/Scripts/Obstacles Gimmick/Temp_Player.cs
@@ -10,23 +10,7 @@ public enum PlayerType
 
 public class Temp_Player : MonoBehaviour
 {
-    public float speed = 5.0f;
     public PlayerType type = PlayerType.Fire;
-
-    void Update()
-    {
-        // 'Horizontal' 축 입력 값 (왼쪽/오른쪽 방향키)
-        float horizontalInput = Input.GetAxis("Horizontal");
-
-        // 'Vertical' 축 입력 값 (위/아래 방향키)
-        float verticalInput = Input.GetAxis("Vertical");
-
-        // 입력 값에 따라 이동 방향 벡터 생성
-        Vector3 moveDirection = new Vector3(horizontalInput, 0, verticalInput);
-
-        // 캐릭터 이동
-        transform.Translate(moveDirection * speed * Time.deltaTime);
-    }
 
     public void PlayerDead()
     {
