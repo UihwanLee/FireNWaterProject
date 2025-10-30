a5ee2ffd29b3741270548b66336ba64475252a26
shin0112 <wnslcosltimo12@gmail.com>
2025-10-30 17:57:11 +0900

    feat: 스테이지 정보 담는 클래스 만들기

--- Assets/Scripts/Stage/Stage.cs
+++ Assets/Scripts/Stage/Stage.cs
@@ -1,8 +1,37 @@
+public enum StageScore
+{
+    None, // Default
+    A, // 조건 전부 통과
+    B, // 조건 2개만 통과
+    C, // 조건 1개만 통과
+}
+
 [System.Serializable]
 public class Stage
 {
-    public int StageNumber { get; }
-    public float LimitTime { get; }
+    public int StageNumber { get; private set; }
+    public float LimitTime { get; private set; }
     public int ElementCount { get; set; }
+
+    public Stage(int number, float limitTime, int elementCount)
+    {
+        StageNumber = number;
+        LimitTime = limitTime;
+        ElementCount = elementCount;
+    }
+}
+
+[System.Serializable]
+public class StageClearInfo
+{
+    public int StageNumber { get; set; }
+    public StageScore StageScore { get; set; }
     public float ClearTime { get; set; }
+
+    public StageClearInfo(Stage stage)
+    {
+        StageNumber = stage.StageNumber;
+        StageScore = StageScore.None;
+        ClearTime = 0f;
+    }
 }
