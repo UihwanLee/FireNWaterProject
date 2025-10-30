fd535fab8b46ad0a4351965881efc8bb66dee51f
shin0112 <wnslcosltimo12@gmail.com>
2025-10-30 12:51:58 +0900

    feat: GameManager 초기화, 필드 넣고, 프리팹에서 연결하기

--- Assets/Scripts/Manager/GameManager.cs
+++ Assets/Scripts/Manager/GameManager.cs
@@ -11,14 +11,37 @@ public enum GameState
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
+    private static GameManager _instance;
+    public static GameManager Instance => _instance;
 
+    // 추구 manager 추가 예정
+    [Header("Managers")]
+    [SerializeField] private ScoreManager _scoreManager;
+
+    private GameState _currentGameState = GameState.None;
+    public GameState CurrentGameState => _currentGameState;
+
+
+    private void Awake()
+    {
+        // 매니저 초기화 (씬 전환 시 중복 방지를 위한 초기화 & 씬 전환 시 삭제 x)
+        if (_instance != null && _instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        _instance = this;
+        DontDestroyOnLoad(gameObject);
+
+        if (_scoreManager == null)
+        {
+            _scoreManager = GetComponentInChildren<ScoreManager>();
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+
+    public void StartGame()
     {
 
     }
