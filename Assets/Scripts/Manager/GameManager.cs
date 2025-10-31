16e6e31ef65a4ccd92325619f951bedc22ca1a12
shin0112 <wnslcosltimo12@gmail.com>
2025-10-31 09:15:56 +0900

    feat: change state 만들기

--- Assets/Scripts/Manager/GameManager.cs
+++ Assets/Scripts/Manager/GameManager.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
@@ -8,6 +9,10 @@ public class GameManager : MonoBehaviour
     // 추후 매니저 추가 예정
     [Header("Managers")]
     [SerializeField] private ScoreManager _scoreManager;
+
+    private GameState _currentGameState = GameState.None;
+    public GameState CurrentGameState => _currentGameState;
+    public event Action<GameState> OnStageChanged;
 
     private void Awake()
     {
@@ -32,5 +37,14 @@ public class GameManager : MonoBehaviour
     {
         // 스테이지 맵으로 전환하기
         // if 튜토리얼을 하지 않았다면 튜토리얼 스테이지로 아니라면 맵으로
+    }
+
+    public void ChangeGameState(GameState gameState)
+    {
+        if (CurrentGameState == gameState) return;  // 동일한 상태일 경우 스킵
+
+        _currentGameState = gameState;
+        OnStageChanged?.Invoke(_currentGameState);
+        Logger.Log($"상태 변경: {_currentGameState}");
     }
 }
