7f13bbce0ec090be349a015284b34d8bc2977527
UihwanLee <luh5063014@naver.com>
2025-11-02 22:08:14 +0900

    feat: Customizing 기본 골격 구현

--- Assets/Scripts/Customizing/CustomizingManager.cs
+++ Assets/Scripts/Customizing/CustomizingManager.cs
@@ -2,17 +2,52 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum CustomizingType
+{
+    Ember,
+    Wade,
+}
+
 public class CustomizingManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    /*
+     * Customizing UI
+     * 
+     * #1.SO로 Customizing Color Option 관리
+     * #2. Ember / Wade Materail 속성을 변경하여 커스터마이징
+     * 
+     */
+
+    [Header("GameObjects")]
+    [SerializeField] private GameObject colorSlotPrefab;
+    [SerializeField] private GameObject avatar_ember;
+    [SerializeField] private GameObject avatar_wade;
+
+    [Header("Transform Info")]
+    [SerializeField] private Transform emberColorSlotParent;
+    [SerializeField] private Transform wadeColorSlotParent;
+
+    [Header("Customzing Data")]
+    [SerializeField] private List<GameObject> emberColorDataList;
+    [SerializeField] private List<GameObject> wadeColorDataList;
+
+    private void Start()
     {
-        
+        GenerateColorSlots();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void GenerateColorSlots()
     {
-        
+
+    }
+
+    public void ChoiceColor()
+    {
+
+    }
+
+    public void PurchaseColor()
+    {
+
     }
 }
