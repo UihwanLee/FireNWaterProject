8a903ad795bc1b87d1a11991f86c1feafef8e2bf
lachy75127873-oss <lachy75127873@gmail.com>
2025-11-03 23:09:38 +0900

    feat: 스테이지 출구 구현 및 클리어 로직 구현, 프리펩화 진행

--- Assets/Scripts/Obstacles Gimmick/Exit/ExitEmber.cs
+++ Assets/Scripts/Obstacles Gimmick/Exit/ExitEmber.cs
@@ -1,20 +1,35 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class ExitEmber : MonoBehaviour, InteractWithController
 {
-    
+    ExitCalculator exitCalculator;
+
+    private void Awake()
+    {
+        exitCalculator = GetComponentInParent<ExitCalculator>();
+        
+    }
+
     public void Activate(BaseController bc)
     {
         if (bc.gameObject.layer == LayerMask.NameToLayer("Ember"))
         {
-            //게임 매니저에 "엠버 진입" 신호
+            exitCalculator.AddPlayer(bc);
+            exitCalculator.IsStageOver();
         }
     }
 
-    public void Deactivate()
+    public void Deactivate(BaseController bc)
     {
-        // 게임 매니저에 "엠버 탈주" 신호
+        if (bc.gameObject.layer == LayerMask.NameToLayer("Ember"))
+        {
+            exitCalculator.RemovePlayer(bc);
+        }
     }
+
+    
+    
 }
