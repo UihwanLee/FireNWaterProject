6a5eeea8af85369d7d53c4700642eabd6ff5b2e4
jaeapark <qtomeoh@kakao.com>
2025-11-03 20:26:17 +0900

    fix:스크립트 Addlistener로 관리 변경

--- Assets/Scripts/UI/SceneButton.cs
+++ Assets/Scripts/UI/SceneButton.cs
@@ -1,13 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SceneButton : MonoBehaviour
 {
+    public enum ButtonType { LoadScene, ExitGame }
+
+    public ButtonType buttonType;
     public SceneController.SceneType targetScene;
 
-    public void OnClickLoadScene()
+    private UnityEngine.UI.Button btn;
+
+    private void Awake()
     {
-        SceneController.Instance.LoadScene(targetScene);
+        btn = GetComponent<UnityEngine.UI.Button>();
+        if (!btn) return;
+
+        btn.onClick.AddListener(OnButtonClick);
+    }
+
+    private void OnButtonClick()
+    {
+        if (buttonType == ButtonType.LoadScene)
+            SceneController.Instance.LoadScene(targetScene);
+        else
+            SceneController.Instance.ExitGame();
     }
 }
