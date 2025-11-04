23c0d79cfe86efa1b197c2fa9e4c671e57d14ad2
UihwanLee <luh5063014@naver.com>
2025-11-04 17:48:45 +0900

    fix: Pulley 오브젝트 init 호출 시 pos, rot 초기화

--- Assets/Scripts/Obstacles Gimmick/Pulley/Pulley.cs
+++ Assets/Scripts/Obstacles Gimmick/Pulley/Pulley.cs
@@ -2,6 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public struct OriginTransform
+{
+    public Vector3 position;
+    public Quaternion rotation;
+
+    public OriginTransform(Vector3 pos, Quaternion rot)
+    {
+        this.position = pos;
+        this.rotation = rot;
+    }
+}
+
 public class Pulley : MonoBehaviour, InteractWithController, IObstacle
 {
     [SerializeField] private GameObject chainPrefab;
@@ -12,13 +24,19 @@ public class Pulley : MonoBehaviour, InteractWithController, IObstacle
     [SerializeField] private Vector2 stoolScale = new Vector2(1.6f, 1f);
 
     private List<HingeJoint2D> pulleyObjects = new List<HingeJoint2D>();
+    private List<OriginTransform> originTransforms = new List<OriginTransform>();
 
     public void Init()
     {
-        foreach(var obj in pulleyObjects)
+        for (int i= 0; i<pulleyObjects.Count; i++)
         {
-            Rigidbody2D rb = obj.GetComponent<Rigidbody2D>();
+            Rigidbody2D rb = pulleyObjects[i].GetComponent<Rigidbody2D>();
             rb.velocity = Vector2.zero;
+
+            pulleyObjects[i].transform.localPosition = originTransforms[i].position;
+            pulleyObjects[i].transform.localRotation = originTransforms[i].rotation;
+
+            Debug.Log("초기화" + originTransforms[i].position);
         }
     }
 
@@ -38,6 +56,9 @@ public class Pulley : MonoBehaviour, InteractWithController, IObstacle
         float extraHeight = -0.45f;
         float finalHeight = 0.0f;
 
+        Vector3 originPos;
+        Quaternion originRot;
+
         Rigidbody2D prevRb = anchor.GetComponent<Rigidbody2D>();
 
         for (int i=0; i< chainCount; i++)
@@ -53,6 +74,12 @@ public class Pulley : MonoBehaviour, InteractWithController, IObstacle
             prevRb = currentRb;
 
             pulleyObjects.Add(chain);
+
+            originPos = chain.transform.localPosition;
+            originRot = chain.transform.localRotation;
+            originTransforms.Add(new OriginTransform(originPos, originRot));
+
+            Debug.Log("초기 생성" + originPos);
         }
 
         HingeJoint2D stool = Instantiate(stoolPrefab, anchor).GetComponent<HingeJoint2D>();
@@ -61,5 +88,9 @@ public class Pulley : MonoBehaviour, InteractWithController, IObstacle
         stool.connectedBody = prevRb;
 
         pulleyObjects.Add(stool);
+
+        originPos = stool.transform.localPosition;
+        originRot = stool.transform.localRotation;
+        originTransforms.Add(new OriginTransform(originPos, originRot));
     }
 }
