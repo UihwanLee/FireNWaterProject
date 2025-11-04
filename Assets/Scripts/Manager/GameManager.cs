c03d650fc332e09782170c125d7b2e127cb57635
shin0112 <wnslcosltimo12@gmail.com>
2025-11-04 21:13:06 +0900

    feat: 배경 오디오 채우기

--- Assets/Scripts/Manager/GameManager.cs
+++ Assets/Scripts/Manager/GameManager.cs
@@ -125,6 +125,8 @@ public class GameManager : MonoBehaviour
         _stageManager.SelectStage(id);
         Logger.Log("젬 확인 델리게이트 추가");
         _scoreManager.OnCheckGemCount += _stageManager.HandleCheckGemCount;
+
+        _audioManager.ChangeBackGroundMusic(Define.BGM_INPLAY);
     }
 
     public void StartStage()
@@ -166,6 +168,8 @@ public class GameManager : MonoBehaviour
         _stageManager.ChangeGameState(GameState.Exit);
         Logger.Log("젬 확인 델리게이트 제거");
         _scoreManager.OnCheckGemCount -= _stageManager.HandleCheckGemCount;
+
+        _audioManager.ChangeBackGroundMusic(Define.BGM_INTRO);
     }
 
     private void HandleStageClear()
